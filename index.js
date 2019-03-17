66893442e2e087ba8a0e43dc2130ddb39e0211d7
Maryam Joseph <josephmaryam.mj@gmail.com>
2019-03-17 18:51:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ function happyHolidays() {
 return "Happy holidays!";
 }
 
-function happyHolidaysTo(name) {
-  console.log('Happy holidays, ${name}' === "Happy holidays, you!");
-  return "Happy holidays, you!";
-}
-
 function happyHolidaysTo(holiday, name) {
-  console.log("Happy ${holiday}, ${name}!" === "Happy Eid, Kaba!");
+  if('Happy holidays, ${name}' === "Happy holidays, you!");
+  return "Happy holidays, you!";
+  else if(happyHolidaysTo(${holiday}, ${name})!" === "Happy Eid, Kaba!");
   return "Happy Eid, Kaba!";
 }
